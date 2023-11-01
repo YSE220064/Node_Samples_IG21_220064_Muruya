a38135a52de24aae27794d784a7e83da311b6c7b
220064@YSE <220064md@yse-c.net>
2023-11-01 12:12:33 +0900

    loginsessions

--- express_sample/controllers/UserController.js
+++ express_sample/controllers/UserController.js
@@ -1,6 +1,22 @@
 
 //トップページ
 exports.index = (req, res) => {
-    // views/user/index.ejs を表示
-    res.render('user/index')
+    //ユーザのセッションを取得
+    const user = req.session.authUser
+
+    //ログインユーザがいれば
+    if (user) {
+        // views/user/index.ejs を表示
+        res.render('user/index', { user: user })
+    } else {
+        //ログインユーザがいなければ、ログインページにリダイレクト
+        res.redirect('/login')
+    }
+}
+
+exports.logout = (req, res) => {
+    //ユーザのセッションを削除
+    delete (req.session.authUser)
+    //ログインページにリダイレクト
+    res.redirect('/login')
 }
